1c7137eda05b7e102cd9787b450a3e51488098e1
an2xgun <nanggun86@gmail.com>
2025-11-22 20:52:09 +0700

    Add files via upload

---
+++ apotik_retail.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.app')
+
+@section('content')
+<h3>🏪 Apotik Retail</h3>
+
+<table class="table table-bordered table-striped">
+    <tr>
+        <th>Nama Obat</th>
+        <th>Harga Jual</th>
+        <th>Stok</th>
+        <th>Aksi</th>
+    </tr>
+
+    @foreach($obat as $row)
+    <tr>
+        <td>{{ $row->nama_obat }}</td>
+        <td>{{ number_format($row->harga_jual) }}</td>
+        <td>{{ $row->stok }}</td>
+        <td>
+            <a href="{{ route('retail.edit', $row->id) }}" class="btn btn-warning btn-sm">Edit</a>
+        </td>
+    </tr>
+    @endforeach
+</table>
+@endsection
